0142ee3c1fa1ce001bfe232039c2c3b0f12a4f8f
rovicc <53498096+rovicc@users.noreply.github.com>
2019-10-14 17:45:48 -0700

    Update Encrypt.java

--- Encrypt.java
+++ Encrypt.java
@@ -8,32 +8,32 @@ import java.io.FileWriter;
 
 
 public class Encrypt {
-	public static void main(String args[]) throws IOException{
+	public static void main(String[] args) throws IOException{
 		Scanner sc = new Scanner(System.in);
-		ArrayList<String> words = new ArrayList<String>();
+		ArrayList<String> words = new ArrayList<String>();//creates an arraylist
 		
 		String input = "";
 		
 		do
 		{
-			input = sc.nextLine();
-			words.add(input);
+			input = sc.nextLine();//read line
+			words.add(input);//add input to array
 		}
-		while (!input.equals(":q"));
+		while (!input.equals(":q"));//while the input is not equals to ":q" loop will continue
 		{
 		
-			for(String s : words)
+			for(String s : words)//gets the words in the arraylist
 			{
-				String encrypt = Base64.getEncoder() .encodeToString(s.getBytes());
+				String encrypt = Base64.getEncoder() .encodeToString(s.getBytes());//encrypt words into base64
 				System.out.println ("Do you want Save or Discard?");
-				String choose = sc.nextLine();
+				String choose = sc.nextLine();//read line
 				
-				if(choose.equals("Save"))
+				if(choose.equals("Save"))//if choose is equals to "Save"
 				{
-					FileWriter fw = new FileWriter( encrypt+".jgh");
+					FileWriter fw = new FileWriter( encrypt+".jgh");//write a new file
 					System.out.println("Input File name:");
-					fw.write(sc.nextLine());
-					break;
+					fw.write(sc.nextLine());//input file name
+					break;//to stop the if statement
 				}	
 				else
 				{
