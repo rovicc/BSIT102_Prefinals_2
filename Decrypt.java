adebfff30d9969920a9e16c0864f99e172bd8740
rovicc <53498096+rovicc@users.noreply.github.com>
2019-10-14 17:45:40 -0700

    Update Decrypt.java

--- Decrypt.java
+++ Decrypt.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class Decrypt {
 	public static void main(String[] args) throws Exception{
-			
-			System.out.println("Enter the file name :");
 			Scanner sc = new Scanner(System.in);
-			
+		
+			System.out.println("Enter the file name :");
+			//gets the file that the user wants to decrypt
 			File file = new File(sc.nextLine() + ".jgh");
 			sc = new Scanner(file);
-			
+		
+			//creates new lines
 			String newline = sc.nextLine();
 			System.out.println(newline);
 			
